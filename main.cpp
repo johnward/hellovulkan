e49cac0d3d0fcdecc980d5f5a679df0d768ec222
John Ward <john@johnward.net>
2022-09-21 22:24:30 +0100

    sewarch the extentions

--- main.cpp
+++ main.cpp
@@ -80,6 +80,21 @@ private:
         {
             throw std::runtime_error("failed to create instance!");
         }
+
+        // Get number of extentions available
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+        // Now get the actual extensions
+        std::vector<VkExtensionProperties> extensions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extensions.data());
+
+        std::cout << "available extensions:\n";
+
+        for (const auto& extension : extensions) 
+        {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
     }
 
     void mainLoop()
